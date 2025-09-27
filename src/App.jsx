97907b2e424853e384cc5863ebd9cdc8c9686e6f
Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>
2025-09-27 13:22:56 +0530

    chadcn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@/components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='bg-amber-700 max-w-sm'>btao ankush bhai</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center gap-4 min-h-screen justify-center">
+      <h1 className="text-2xl font-bold">Shadcn UI Button Demo</h1>
+      <Button>Primary Button</Button>
+      <Button variant="outline">Outline Button</Button>
+      <Button variant="destructive">Destructive Button</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
