67eb16003ed69b979ecdcddc6772f73ce6e78899
chakresh chakshu <sharma.ankush9193@gmail.com>
2025-10-10 18:29:16 +0530

    added why choose skillyards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import OurPartner from '@/components/OurPartners';
 import Stats from '@/components/Stats';
 import WhatStudentsSaid from '@/components/feedback-home/WhatStudentsSaid';
 import useScrollAnimation from '../components/hooks/useScrollAnimation';
+import WhyChooseSkillyards from '@/components/whyChooseSkillyards';
 
 
 const Home = () => {
@@ -15,10 +16,7 @@ const Home = () => {
       <OurPartner className ={"reveal"} />
       <WhatStudentsSaid  className={"reveal"}/>
       <Stats />
-      <div className="container mx-auto px-6 py-8 pt-28 reveal">
-        <h1 className="text-3xl font-bold text-blue-950">Welcome to Home</h1>
-        <p>This is the home page.</p>
-      </div>
+      <WhyChooseSkillyards/>
     </>
   );
 };
